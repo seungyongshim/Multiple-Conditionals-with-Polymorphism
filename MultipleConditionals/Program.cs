5ddb223858f8675a62384129a84d416000103eb9
심승용 <seungyong.shim@mirero.co.kr>
2019-05-19 10:46:03 +0900

    다수의 플래그를 가진 클래스 예시

---
+++ MultipleConditionals/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MultipleConditionals
+{
+    public class ClassWithConditionals
+    {
+        private bool _conditional1;
+        private EnumeratedType _conditional2;
+
+        public ClassWithConditionals(bool cond1, EnumeratedType cond2)
+        {
+            _conditional1 = cond1;
+            _conditional2 = cond2;
+        }
+
+        public void Method1()
+        {
+            if (_conditional1)
+            {
+                //do something
+            }
+            else
+            {
+                //do something else
+            }
+        }
+
+        public void Method2()
+        {
+            switch (_conditional2)
+            {
+                case EnumeratedType.CASE1:
+                    //do something
+                    break;
+                case EnumeratedType.CASE2:
+                    //do something else
+                    break;
+                case EnumeratedType.CASE3:
+                    //do something entirely different
+                    break;
+            }
+        }
+    }
+
+    public enum EnumeratedType
+    {
+        CASE1,
+        CASE2,
+        CASE3
+    }
+
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
